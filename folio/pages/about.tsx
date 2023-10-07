a7f708b853460fb45f09f10aa390d7d6cd6e4d30
Andrew J <jumanca@uw.edu>
2023-10-06 18:09:23 -0700

    Added portfolio logo

--- folio/pages/about.tsx
+++ folio/pages/about.tsx
@@ -9,17 +9,17 @@ const About: React.FC = () => {
         <p>
             I&apos;m a 22-year-old software developer actively seeking employment in full-stack and data engineer roles. My main
             areas of expertise are in server-side development and testing, API design, and pipeline engineering for data-related
-            applications, particularly machine learning. I have more specific details about my  experiences and skill areas in my 
-            <Link href={"/projects/"}>projects</Link> and <Link href={"/experiences/"}>experiences</Link> sections.
+            applications, particularly machine learning. I have more specific details about my  experiences and skill areas in my
+            <Link href={"/projects/"}> projects</Link> and <Link href={"/experiences/"}>experiences</Link> pages.
         </p>
         <p>          
         </p>
         <p>
             But first, why did I choose software development, and what part of software am I even involed in? In short, my answer now
             will probably be different in a few years, or even months. To be frank, even the software and frameworks used to write this 
-            very website has probably had multiple patches or new features since I last touched it. My point is, I didn&apos;t get into writing
-            software because I had an end goal in mind; I got into software because at its core, it is all logic, design, and implementation, 
-            no matter what branch of technology you&apos;re working in.             
+            very website have probably had multiple patches or new features since my last use. My point is, I didn&apos;t get into writing
+            software because I had an end goal in mind; I got into software because at its core, it is all about logic, design, and implementation, 
+            regardless of what branch of technology you&apos;re working in.             
             
         </p>
         <p>
